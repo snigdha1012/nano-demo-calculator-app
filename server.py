7b5b1356c9743101e71a8e6a3777d84aba71c35d
Snigdha Paladugula <snigdha1012@gmail.com>
2023-08-23 17:02:56 +0530

    Commit

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask, request, jsonify
+from flask import Flask , request, jsonify
 
-app = Flask(__name__)
+app = Flask(_name_)
+
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return "hello world!", 200
+    return "Hello world!", 200
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
@@ -12,8 +13,7 @@ def add():
     first = data['first']
     second = data['second']
     result = first + second
-    response = {'result': result}
-    return jsonify(response), 200
+    return jsonify(result=result), 200
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
@@ -21,8 +21,7 @@ def subtract():
     first = data['first']
     second = data['second']
     result = first - second
-    response = {'result': result}
-    return jsonify(response), 200
+    return jsonify(result=result), 200
 
-if __name__ == '__main__':
-    app.run(port=8080, host='0.0.0.0')
+if _name_ == '_main_':
+    app.run(port=8080,host='0.0.0.0')
