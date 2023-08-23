3a386e46bc48dbb5bd9489cae91c444dac38ea18
Snigdha Paladugula <snigdha1012@gmail.com>
2023-08-23 16:17:54 +0530

    comitted1

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return "Hello world!", 200
+    return "hello world!", 200  # Note: changed to lowercase "hello"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
