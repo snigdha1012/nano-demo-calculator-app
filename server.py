9f6dfe6c7d94e5f7a40768955d92690c4ac63ccb
Snigdha Paladugula <snigdha1012@gmail.com>
2023-08-23 11:26:28 +0530

    comitted

---
+++ server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/calculator/greeting", methods=['GET'])
+def greeting():
+    return 'hello'
+
+@app.route("/calculator/add", methods=['POST'])
+def add():
+    return 'a+b'
+
+@app.route("/calculator/subtract", methods=['POST'])
+def subtract():
+    return '-b'
+
+if __name__ == '__main__':
+    app.run(port=8080,host='0.0.0.0')
